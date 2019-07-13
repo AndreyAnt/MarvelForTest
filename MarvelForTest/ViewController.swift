8284d922c8ede8b29cb74d7d88ec2087f334c75d
Andrey Ant <antropov0712@gmail.com>
2019-07-13 13:54:56 +0300

    Initial commit

---
+++ MarvelForTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MarvelForTest
+//
+//  Created by user on 13/07/2019.
+//  Copyright © 2019 AAA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
